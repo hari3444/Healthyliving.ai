48a26777b166108e0447b92ada6511178d6f1ee1
hari3444 <harikollu22@gmail.com>
2023-10-07 12:41:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/daily-routine">Daily Routine</Link>
+          <Link to="/vegan">Vegan</Link>
+          <Link to="/vegetarian">Vegetarian</Link>
+          <Link to="/non-vegetarian">Non-Vegetarian</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/daily-routine" element={<DailyRoutine />} />
+          <Route path="/vegan" element={<Vegan />} />
+          <Route path="/vegetarian" element={<Vegetarian />} />
+          <Route path="/non-vegetarian" element={<NonVegetarian />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+const Welcome = () => {
+  return (
+    <h1 className="welcome">Welcome to Healthyliving.ai!</h1>
+  );
+};
+
+const DailyRoutine = () => {
+  return (
+    <div>
+      <h1>Daily Routine</h1>
+      <p>This is the daily routine page.</p>
     </div>
   );
-}
+};
+
+const Vegan = () => {
+  return (
+    <div>
+      <h1>Vegan</h1>
+      <p>This is the vegan page.</p>
+    </div>
+  );
+};
+
+const Vegetarian = () => {
+  return (
+    <div>
+      <h1>Vegetarian</h1>
+      <p>This is the vegetarian page.</p>
+    </div>
+  );
+};
+
+const NonVegetarian = () => {
+  return (
+    <div>
+      <h1>Non-Vegetarian</h1>
+      <p>This is the non-vegetarian page.</p>
+    </div>
+  );
+};
 
 export default App;
